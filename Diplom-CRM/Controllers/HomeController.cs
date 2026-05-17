06bb7421f8dc1407bd3166fc41db26218224a3fd
k1rer <ghas_00@mail.ru>
2026-05-17 21:31:26 +0700

    Добавлены начальные файлы шаблона

---
+++ Diplom-CRM/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Diplom_CRM.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Diplom_CRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Diplom-CRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Diplom_CRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
